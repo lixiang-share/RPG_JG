e12f3d2d59ebcb7d50e4fb1f152d51bbbb7b2677
9guang <780632213@qq.com>
2015-12-08 18:57:21 +0800

    build luaComponent

---
+++ Assets/Scripts/Common/LuaComponent.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LuaComponent : MonoBehaviour {
+
+    public string filename;
+    public void Awake()
+    {
+        InitLuaFile(filename);
+    }
+
+    private void InitLuaFile(string filename)
+    {
+        
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void OnEnable()
+    {
+
+    }
+
+    public void OnDisable()
+    {
+
+    }
+
+    public void OnDestroy()
+    {
+
+    }
+}
