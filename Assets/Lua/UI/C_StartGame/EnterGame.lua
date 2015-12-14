1280a94d97ac7de43fcd613bbdf8098fc787d860
9guang <780632213@qq.com>
2015-12-14 20:59:02 +0800

    enter and select role

---
+++ Assets/Lua/UI/C_StartGame/EnterGame.lua
@@ -0,0 +1,35 @@
+﻿EnterGame = {};
+local this = EnterGame;
+local inst;
+
+
+function this.Awake()
+	inst = this.inst;
+end
+
+function this.Start()
+	
+end
+
+
+function  this.OnCommand(command , param)
+	if command == "ChangeRole" then
+		UITools.Log("3333");
+		inst:OnCommand("MoveLeft");
+		UITools.D("SelectRole"):OnCommand("MoveCenter");
+	elseif command == "MoveLeft" then
+		UITools.Log("1111");
+		inst['pos'] = 'Left';
+		UITools.TweenPos_X(inst,-780);
+		UITools.Log("222");
+	elseif command == "MoveCenter" then
+		UITools.SA(inst,true);
+		inst['pos'] = 'Center';
+		UITools.TweenPos_X(inst,0);
+	elseif command == "EndTween" then
+		if inst['pos'] == 'Left' then
+			UITools.SA(inst,false);
+		end
+	end
+end
+
---
+++ Assets/Lua/UI/C_StartGame/SelectRole.lua
@@ -0,0 +1,40 @@
+﻿SelectRole = {};
+local this = SelectRole;
+local inst;
+
+
+function this.Awake()
+	inst = this.inst;
+end
+
+function this.Start()
+	
+end
+
+
+
+function  this.OnCommand(command , param)
+	if command == "Return" then
+		UITools.Log("3333");
+
+		inst:OnCommand(inst,"Right");
+		UITools.D("SelectRole"):OnCommand("MoveCenter");
+
+	elseif command == "Right" then
+		UITools.Log("1111");
+		inst['pos'] = 'Right';
+		UITools.TweenPos_X(inst,836);
+		UITools.Log("222");
+
+	elseif command == "MoveCenter" then
+		UITools.SA(inst,true);
+		inst['pos'] = 'Center';
+		UITools.TweenPos_X(inst,0);
+	elseif command == "EndTween" then
+		if inst['pos'] == 'Right' then
+			UITools.SA(inst,false);
+		end
+	end
+end
+
+
