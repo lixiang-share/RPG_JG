e9c93094d24b09786b5fafdb37c1d278fce9d0a9
jiuguang <780632213@qq.com>
2016-01-27 23:06:27 +0800

    server log DB pool and dbUtils

--- RPG_Server/JWorkSpace/JServer/src/jg/rpg/entity/Player.java
+++ RPG_Server/JWorkSpace/JServer/src/jg/rpg/entity/Player.java
@@ -1,5 +1,15 @@
 package jg.rpg.entity;
 
-public class Player {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.commons.dbutils.ResultSetHandler;
+
+public class Player implements ResultSetHandler{
+
+	@Override
+	public Object handle(ResultSet rs) throws SQLException {
+		return null;
+	}
 
 }
