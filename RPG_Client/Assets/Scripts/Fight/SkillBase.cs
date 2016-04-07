1a220f84edc5fae7c3b35cbb7995ad9dfeff49b9
jiuguang <780632213@qq.com>
2016-04-07 18:33:27 +0800

    begain skill sys

---
+++ RPG_Client/Assets/Scripts/Fight/SkillBase.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillBase : ScaleButton {
+    public SkillItem skillInfo;
+    public bool isInit = false;
+    public bool isCD = false;
+    public UISprite cdSprite;
+    private float curCDTime;
+
+    public override void Start()
+    {
+        base.Start();
+        if (cdSprite == null) cdSprite = this.GetChild("s_fore").GetComponent<UISprite>();
+    }
+
+    public void Update()
+    {
+        if (isCD)
+        {
+            if(curCDTime <= 0) { EndCD(); return; }
+            curCDTime = curCDTime - Time.deltaTime;
+            SetCDProgress(); 
+        }
+    }
+
+    public void EndCD()
+    {
+        isCD = false;
+        curCDTime = 0f;
+        cdSprite.gameObject.SetActive(false);
+    }
+
+
+    public virtual void ReviceSkillInfo(SkillItem skill)
+    {
+        this.skillInfo = skill;
+        InitState();
+    }
+
+    public void SetCDProgress()
+    {
+        if (skillInfo.Type == "Base" || skillInfo.ColdTime < 0.001f) return;
+        float progress = curCDTime / skillInfo.ColdTime;
+        progress = Mathf.Clamp01(progress);
+        if(progress <=0.001F)
+        {
+            cdSprite.gameObject.SetActive(false);
+        }
+        else
+        {
+            if (!cdSprite.gameObject.activeSelf) cdSprite.gameObject.SetActive(true);
+            cdSprite.fillAmount = progress;
+        }
+    }
+
+    public void InitState()
+    {
+        this.GetChild("s_icon").Value = this.skillInfo.Icon;
+        if (skillInfo.Type == "Base") cdSprite.gameObject.SetActive(false);
+        curCDTime = 0;
+        isCD = true;
+        isInit = true;
+    }
+    public void EnterCD()
+    {
+        isCD = true;
+        curCDTime = skillInfo.ColdTime;
+        cdSprite.gameObject.SetActive(true);
+    }
+
+    public override void OnClick()
+    {
+        if (!isCD) EnterCD();
+    }
+}
---
+++ RPG_Client/Assets/Scripts/Fight/SkillManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class SkillManager : LuaBehaviour {
+
+    public bool isInit;
+    public List<SkillItem> skills;
+
+	void Start () {
+        List<SkillItem> skills = new List<SkillItem>();
+        SkillItem skill = new SkillItem();
+        skill.ColdTime = 1;
+        skill.Icon = "icon_li";
+        this.GetChild("Skill-item").GetComponent<SkillBase>().ReviceSkillInfo(skill);
+	}
+
+
+    public void InitState(List<SkillItem> skills)
+    {
+        this.skills = skills;
+        foreach(SkillItem skill in skills)
+        {
+            
+        }
+    }
+
+
+	
+    public void ReleaseSkill(int skillID , DefAction OnSuccess=null , DefAction OnFail = null)
+    {
+
+    }
+}
